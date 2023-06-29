b25de19b1d30d7d4092423c5ec8cde602131dd0a
Ceren Şahin <cerennssahinn@gmail.com>
2023-06-29 23:44:18 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -3,7 +3,30 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
+const hearts = document.querySelectorAll("li");
+hearts.forEach(heart => {
+  console.log(heart);
+  heart.addEventListener("click", () => {
+    mimicServerCall()
+    .then(() => {
+      if (heart.classList.contains("activated-heart")) {
+        heart.textContent = `Like! ${EMPTY_HEART}`;
+        heart.classList.remove("activated-heart");
+      }
+      else {
+        heart.textContent = FULL_HEART;
+        heart.classList.add("activated-heart");
+      }
+    })
+    .catch((error) => {
+      const errorContainer = document.getElementById("modal");
+      errorContainer.classList.remove("hidden");
+      setTimeout(() => errorContainer.classList.add("hidden"), 3000);
+      const errorMsg = document.getElementById("modal-message");
+      errorMsg.textContent = error;
+    });
+  });
+}) 
 
 
 
